23788b055762a5e636c10d53e703f3146c78e97e
viniciusoliveira5879-debug <>
2026-03-29 19:00:36 +0000

    Boolean Operators

---
+++ conditional_statements/boolean_operators/main.py
@@ -0,0 +1,8 @@
+# Passo 1: definir total_cost
+total_cost = 25.00
+
+# Passo 2: criar a variável boolena corretamente
+discountEligible = total_cost  >= 20.00
+
+#Passo 3: imprimir com sintaxe correta
+print( "Is the purchase eligible for a discount?", discountEligible)
